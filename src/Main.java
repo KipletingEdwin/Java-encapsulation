ff8fdac7fe91e49bbfcecdb57baa282394646d51
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-03-01 15:47:55 +0300

    add setter and getter method

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,16 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //Encapsulation = attributes of a class will be hidden or private,
+        //Can be accessed only through methods(getters 7 setters)
+        //You should make attributes private if you dont have a reason to make them public/protected
+        Car car = new Car("Chevrolet", "Camaro", 2021);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        car.setYear(2024);
+        System.out.println(car.getYear());
+        System.out.println(car.getModel());
+        System.out.println(car.getMake());
+
+
     }
 }
